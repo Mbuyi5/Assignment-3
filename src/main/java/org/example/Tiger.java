3ea6ccc37efad0b7e41e4147a39920db3affb3b5
Mbuyi5 <42034591+Mbuyi5@users.noreply.github.com>
2020-03-12 14:54:28 +0200

    Update Tiger.java

--- src/main/java/org/example/Tiger.java
+++ src/main/java/org/example/Tiger.java
@@ -1,10 +1,123 @@
-package org.example;
 
-    public class Tiger extends Monkeys{
+public interface AnimalInterface {
 
-        @Override
-        public void monkeysSound(){
-            System.out.println("Woof");
-        }
+    public abstract String foodEaten(String food);
+
+}
+
+
+public class DogInheritance extends AnimalInheritance {
+
+
+
+    public DogInheritance(){
+
+        super();
+
+
+        //dh.displayAge();
+
+
     }
+
+
+    public void dogAge(int dogAge){
+
+        DogInheritance dh = new DogInheritance();
+        dh.age(dogAge);
+
+    }
+
+
+
+
+
+
+}
+
+public class DogInheritance2 {
+
+    AnimalInheritance2 animal;
+
+    public DogInheritance2(){
+
+        //super();
+
+
+        //dh.displayAge();
+
+
+    }
+
+
+    public void dogAge(int dogAge){
+
+        DogInheritance dh = new DogInheritance();
+        dh.age(dogAge);
+
+    }
+
+
+}
+
+public class DogPolymorphism extends AnimalPolymorphism implements AnimalInterface{
+
+    public DogPolymorphism(){}
+
+
+    @Override
+    public String movement(String moveType) {
+
+        return "Running movement";
+    }
+
+
+    @Override
+    public String foodEaten(String food) {
+
+        return "Bone";
+    }
+}
+
+public class FishPolymorphism extends AnimalPolymorphism implements AnimalInterface {
+
+    public FishPolymorphism(){}
+
+    @Override
+    public String movement(String moveType) {
+
+        return "Swimming movement";
+    }
+
+    @Override
+    public String foodEaten(String food) {
+
+        return "Worms";
+    }
+}
+
+public class RunAnimal {
+
+
+    public static void main(String [] args){
+
+
+
+    }
+
+}
+
+public class TestOOP {
+
+    public static void main(String [] args){
+
+        //inheritance
+        DogInheritance inher = new DogInheritance();
+        inher.age(1996);
+
+        //Polymorphism
+
+
+    }
+
 }
